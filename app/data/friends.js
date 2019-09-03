29846767d2e726eb7a6ed6f4f34c5b1e6bbc1e79
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-09-03 13:05:34 -0500

    friends.js friend array push

---
+++ app/data/friends.js
@@ -0,0 +1,62 @@
+var friends = [
+    {
+        "name": "Shaquima Johnson-McSwain",
+        "photo": "https://www.facebook.com/photo.php?fbid=102062264370802&set=a.102062287704133&type=3&theater",
+        "scores[]": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+    {
+        "name": "Bre Moulder",
+        "photo": "https://www.instagram.com/p/BvpyJlBl8N7/",
+        "scores[]": [
+            "3",
+            "1",
+            "4",
+            "5",
+            "3",
+            "1",
+            "2",
+            "5",
+            "4",
+            "2"
+
+
+        ]
+
+    },
+    {
+        "name": "Madeline Jefferson",
+        "photo": "https://www.instagram.com/p/B1nKd8ynlFn/",
+        "scores[]": [
+            "5",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "4",
+            "1",
+            "5"
+
+
+
+        ]
+    }
+];
+
+
+
+
+module.exports = friends;
